b6e5102b2b976b11ee2636090ef4024dda3aa4dd
thongnguyen204 <spxdialga@gmail.com>
2021-10-26 16:13:26 +0700

    update font size footer

--- resources/lang/vn/footer.php
+++ resources/lang/vn/footer.php
@@ -5,7 +5,7 @@ return [
 
     'Try'   => 'Chúng tôi nỗ lực mỗi ngày để đem đến những sản phẩm du lịch mới mẻ và chất lượng dịch vụ hoàn hảo tới bạn.',
     'Contacts'  => 'THÔNG TIN LIÊN HỆ',
-    'Links' => 'LIÊN KẾT NHANH',
+    'Links' => 'LIÊN KẾT',
     'Payment'   => 'Phương thức thanh toán',
     'Intro'     => 'Website của chúng tôi',
     'About us'  => 'Về chúng tôi',
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -4,7 +4,7 @@
 
     <div style="max-width: 1300px" class="container">
         <div class="row">
-            <div class="col-12 col-md-4">
+            <div style="margin-top: 50px" class="col-12 col-md-4">
                 <div class="logo">
                     <a class="logo" href="{{ route('home') }}">
                         {{ config('app.name', 'Laravel') }}
@@ -25,26 +25,26 @@
                         <hr>
                     </div>
 
-                    <p>
-                        <i class="bi bi-geo-alt"></i>
-                        97 Võ Văn Tần, Phường 6, Quận 3, Thành phố Hồ Chí Minh
-                    </p>
-
-                    <p>
-                        <i class="bi bi-telephone"></i>
-                        Hotline: 0905200419
-                    </p>
-
-                    <p>
-                        <i class="bi bi-envelope"></i>
-                        1851010131Thong@ou.edu.vn
-                    </p>
-
-                    <p>
-                        <i class="bi bi-envelope"></i>
-                        1851010144Trung@ou.edu.vn
-                    </p>
                 </div>
+                <p>
+                    <i class="bi bi-geo-alt"></i>
+                    97 Võ Văn Tần, Phường 6, Quận 3, Thành phố Hồ Chí Minh
+                </p>
+
+                <p>
+                    <i class="bi bi-telephone"></i>
+                    Hotline: 0905200419
+                </p>
+
+                <p>
+                    <i class="bi bi-envelope"></i>
+                    1851010131Thong@ou.edu.vn
+                </p>
+
+                <p>
+                    <i class="bi bi-envelope"></i>
+                    1851010144Trung@ou.edu.vn
+                </p>
             </div>
             <div class="col-12 col-md-4">
                 <div class="footer-title">
@@ -53,33 +53,33 @@
                         <hr>
                     </div>
 
-                    <p>
-                        <a class="footer-link" href="tel:0905200419">
-                            <i class="bi bi-telephone"></i>
-                            0905200419
-                        </a>
-                    </p>
-
-                    <p>
-                        <a class="footer-link" href="{{route('payment.index')}}">
-                            <i class="bi bi-cash-coin"></i>
-                            {{__('footer.Payment')}}
-                        </a>
-                    </p>
-                    <p>
-                        <a class="footer-link" href="{{route('introduction.index')}}">
-                            <i class="bi bi-people-fill"></i>
-                            {{__('footer.Intro')}}
-                        </a>
-                    </p>
-
-                    <p>
-                        <a class="footer-link" href="">
-                            <i class="bi bi-person-square"></i>
-                            {{__('footer.About us')}}
-                        </a>
-                    </p>
                 </div>
+                <p>
+                    <a class="footer-link" href="tel:0905200419">
+                        <i class="bi bi-telephone"></i>
+                        0905200419
+                    </a>
+                </p>
+
+                <p>
+                    <a class="footer-link" href="{{route('payment.index')}}">
+                        <i class="bi bi-cash-coin"></i>
+                        {{__('footer.Payment')}}
+                    </a>
+                </p>
+                <p>
+                    <a class="footer-link" href="{{route('introduction.index')}}">
+                        <i class="bi bi-people-fill"></i>
+                        {{__('footer.Intro')}}
+                    </a>
+                </p>
+
+                <p>
+                    <a class="footer-link" href="">
+                        <i class="bi bi-person-square"></i>
+                        {{__('footer.About us')}}
+                    </a>
+                </p>
             </div>
         </div>
     </div>
