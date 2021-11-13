01a7142526b4b501d60bd60125984d8e69797443
thongnguyen204 <spxdialga@gmail.com>
2021-11-13 19:42:34 +0700

    update mail function

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -11,8 +11,10 @@ class MailController extends Controller
     public function test(Request $request)
     {
         $input = $request->all();
-        Mail::send('mail/mailfb', array('name'=>$input["name"],'email'=>$input["email"], 'content'=>$input['comment']), function($message){
-	        $message->to('konannguyen6@gmail.com', 'Thanh Trung')->subject('Test mail Laravel !');
+
+        Mail::send('mail/mailfb', array('name'=>$input["name"],'email'=>$input["email"], 'content'=>$input['comment']), function($message) use ($input)
+        {
+	        $message->to($input["email"], 'asd')->subject('Test mail Laravel !');
 	    });
         // Session::flash('flash_message', 'Send message successfully!');
 
